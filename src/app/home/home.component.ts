2c77052ab3d42fcbf49f2753584f7f57c94577fc
Adem <ado.designs@outlook.de>
2025-06-22 23:01:29 +0200

    feat: add placeholders for layout components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
-import { HeroComponent } from './hero/hero.component';
+import { HeroIntroComponent } from '../hero-intro/hero-intro.component';
+import { HeroLetsWorkComponent } from '../hero-lets-work/hero-lets-work.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, HeroComponent],
+  imports: [
+    HeaderComponent,
+    HeroIntroComponent,
+    HeroLetsWorkComponent,
+    SkillsComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
