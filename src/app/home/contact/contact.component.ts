7f7ffeac506cbec2fcbedf09223cfa6494e79efc
Adem <ado.designs@outlook.de>
2025-06-25 01:14:55 +0200

    feat: HTML structure for form

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
-
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
+  onSubmit() {
+    console.log(this.contactData);
+  }
 }
