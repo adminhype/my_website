72e94769f090bf8944fb60bd9343225b6dfcba6f
Adem <ado.designs@outlook.de>
2025-06-29 23:15:34 +0200

    test: verify translation functionality

--- src/app/hero-intro/hero-intro.component.ts
+++ src/app/hero-intro/hero-intro.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-
+import {
+  TranslatePipe,
+  TranslateDirective,
+  TranslateService,
+} from '@ngx-translate/core';
 @Component({
   selector: 'app-hero-intro',
-  imports: [],
+  imports: [TranslatePipe, TranslateDirective],
   templateUrl: './hero-intro.component.html',
-  styleUrl: './hero-intro.component.scss'
+  styleUrl: './hero-intro.component.scss',
 })
 export class HeroIntroComponent {
-
+  constructor(private translate: TranslateService) {}
+  changeLanguage(language: string) {
+    this.translate.use(language);
+  }
 }
